ddb7be51da5decca360306c45784777e063333af
糯米兔 <robin.sh@qq.com>
2024-08-14 11:55:16 +0800

    update bash

--- changeindexhtml.py
+++ changeindexhtml.py
@@ -24,13 +24,14 @@ def change_html():
         html_content
     )
 
-    # 删除</main><footer id="site-footer" class="py-10 print:hidden">到</footer>之间的内容
+    
+    # 在</main><footer id="site-footer" class="py-10 print:hidden">修改为<style>.stylecenter {margin-left: 30%;}</style></main><footer id="site-footer" class="py-10 print:hidden stylecenter">
     html_content = re.sub(
-        r'(<footer id="site-footer" class="py-10 print:hidden">).*?(</footer>)',
-        r'\1\2',
-        html_content,
-        flags=re.DOTALL
+        r'</main><footer id="site-footer" class="py-10 print:hidden">',
+        r'<style>.stylecenter {text-align: center; margin-left: auto; margin-right: auto;}</style></main><footer id="site-footer" class="py-10 print:hidden stylecenter">',
+        html_content
     )
+    
 
 
     # 寻找 <div class="mx-auto max-w-7xl p-0">，替换成<style>.container {max-width: 56rem;}</style><div class="mx-auto container p-0">
